ee84b63549e452affe7fb42fd5231cf4f8641ffa
JSK1379 <js13579K@gmail.com>
2026-09-18 19:33:46 +0800

    refactor(chat): complete feature barrel

--- app/lib/features/chat/chat.dart
+++ app/lib/features/chat/chat.dart
@@ -1,4 +1,7 @@
 export 'models/chat_models.dart';
+export 'pages/chat_page.dart';
+export 'pages/chat_room_list_page.dart';
 export 'services/chat_room_open_manager.dart';
+export 'services/chat_service.dart';
 export 'services/chat_service_singleton.dart';
 export 'services/image_api_service.dart';
