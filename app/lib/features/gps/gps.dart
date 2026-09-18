323c5a615ca93674ffd45b110dae72950ebc5042
JSK1379 <js13579K@gmail.com>
2026-09-18 19:33:48 +0800

    refactor(gps): complete feature barrel

--- app/lib/features/gps/gps.dart
+++ app/lib/features/gps/gps.dart
@@ -1,3 +1,8 @@
 export 'models/gps_models.dart';
+export 'pages/gps_background_settings_page.dart';
+export 'pages/high_frequency_gps_test_page.dart';
+export 'services/background_gps_service.dart';
+export 'services/enhanced_foreground_location_service.dart';
+export 'services/foreground_location_service.dart';
 export 'services/gps_service.dart';
 export 'services/gps_tracker.dart';
